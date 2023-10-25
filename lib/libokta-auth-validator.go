51a14e29800fc4c451385be5ed6474f6ab76b793
Jeremy JACQUE <jeremy.jacque@algolia.com>
2023-10-25 09:37:58 +0200

    style(lib): use gofmt std indentation

--- lib/libokta-auth-validator.go
+++ lib/libokta-auth-validator.go
@@ -3,7 +3,7 @@ package main
 import "C"
 
 import (
-  "gopkg.in/algolia/openvpn-auth-okta.v2/pkg/validator"
+	"gopkg.in/algolia/openvpn-auth-okta.v2/pkg/validator"
 )
 
 type PluginEnv = validator.PluginEnv
@@ -11,20 +11,20 @@ type PluginEnv = validator.PluginEnv
 //export OktaAuthValidator
 func OktaAuthValidator(ctrF *C.char, ip *C.char, cn *C.char, user *C.char, pass *C.char) {
 
-  pluginEnv := &PluginEnv{
-    Username: C.GoString(user),
-    CommonName: C.GoString(cn),
-    Password: C.GoString(pass),
-    ClientIp: C.GoString(ip),
-    ControlFile: C.GoString(ctrF),
-  }
+	pluginEnv := &PluginEnv{
+		Username:    C.GoString(user),
+		CommonName:  C.GoString(cn),
+		Password:    C.GoString(pass),
+		ClientIp:    C.GoString(ip),
+		ControlFile: C.GoString(ctrF),
+	}
 
-  v := validator.NewOktaOpenVPNValidator()
-  if res := v.Setup(true, nil, pluginEnv); !res {
-    return
-  }
-  _ = v.Authenticate()
-  v.WriteControlFile()
+	v := validator.NewOktaOpenVPNValidator()
+	if res := v.Setup(true, nil, pluginEnv); !res {
+		return
+	}
+	_ = v.Authenticate()
+	v.WriteControlFile()
 }
 
 func main() {
