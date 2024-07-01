f3f1a7dc8e38255d352122d9b393f443b3bd0a79
Jeremy JACQUE <jeremy.jacque@algolia.com>
2024-07-01 13:30:04 +0200

    chore(oktaApiAuth): use Chrome on Linux user agent along with Sec-CH headers

--- pkg/oktaApiAuth/oktaApiAuth.go
+++ pkg/oktaApiAuth/oktaApiAuth.go
@@ -30,7 +30,6 @@ func New() *OktaApiAuth {
 			TOTPFallbackToPush:  false,
 		},
 		UserConfig: &OktaUserConfig{},
-		userAgent:  "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/122.0 (OktaOpenVPN)",
 	}
 }
 
