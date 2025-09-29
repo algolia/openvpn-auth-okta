581d76960a2a3c5f62d4c3f0ab913934047587cf
Jeremy JACQUE <jeremy.jacque@algolia.com>
2025-09-29 22:24:29 +0200

    test(duoAuthApi): introduce parseUrl basic test
    Signed-off-by: Jeremy JACQUE <jeremy.jacque@algolia.com>

--- pkg/duoAuthApi/api_test.go
+++ pkg/duoAuthApi/api_test.go
@@ -9,3 +9,29 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 package duoAuthApi
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/algolia/openvpn-auth-okta.v3/pkg/authApi"
+)
+
+func TestDuoParseUrl(t *testing.T) {
+	urlTests := map[string]string{
+		"toto":                 "toto",
+		"toto.com":             "toto.com",
+		"https://toto.com":     "toto.com",
+		"toto.com:443":         "toto.com:443",
+		"https://toto.com:443": "toto.com:443",
+	}
+	apiCfg := &authApi.APIConfig{}
+	a := &DuoAuthApi{ApiConfig: apiCfg}
+	for test, expected := range urlTests {
+		t.Run(fmt.Sprintf("parseUrl(%s)", test), func(t *testing.T) {
+			apiCfg.Url = test
+			assert.Equal(t, expected, a.parseUrl())
+		})
+	}
+}
